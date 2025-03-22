fc7d88df9c3784e22557e07db848a0ae343dda4d
sauvik <sauvik@logic-square.com>
2025-03-22 22:31:43 +0530

    feat: implement authentication checks in layout components to redirect users based on authentication status

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -74,6 +74,7 @@ export const signIn = async (params: SignInParams) => {
     };
   }
 };
+
 export const setSessionCookies = async (idToken: string) => {
   const cookieStore = await cookies(); // storing in cookies using next/cookies
 
@@ -89,4 +90,42 @@ export const setSessionCookies = async (idToken: string) => {
     path: "/",
     sameSite: "lax",
   });
+};
+
+export const getCurrentUser = async (): Promise<User | null> => {
+  const cookieStore = await cookies();
+
+  const sessionCookie = cookieStore.get("session")?.value;
+
+  //   user doesn't exist
+  if (!sessionCookie) {
+    return null;
+  }
+
+  try {
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+
+    const userRecord = await db
+      .collection("users")
+      .doc(decodedClaims.uid)
+      .get();
+
+    if (!userRecord?.exists) {
+      return null;
+    }
+
+    return {
+      ...userRecord?.data(),
+      id: userRecord?.id,
+    } as User;
+  } catch (error) {
+    console.log("Error getting current user", error);
+    return null;
+  }
+};
+
+export const isAuthenticated = async () => {
+  const user = await getCurrentUser();
+
+  return !!user; // if user exists, return true, otherwise return false
 };
