63c34dcbf4dc00eef423aa83f10b1e34740b4443
sauvik <sauvik@logic-square.com>
2025-03-23 00:40:04 +0530

    feat: added vapi support for interview generation with assistant

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,11 +1,19 @@
 import Agent from "@/components/Agent";
+import { getCurrentUser } from "@/lib/actions/auth.action";
+import { redirect } from "next/navigation";
 
-const InterviewPage = () => {
+const InterviewPage = async () => {
+  const user = await getCurrentUser();
+
+  if (!user) {
+    redirect("/login");
+  }
+
   return (
     <>
       <h3>Interview Generation</h3>
 
-      <Agent userName="John Doe" userId="1" type="generate" />
+      <Agent userName={user?.name} userId={user?.id} type="generate" />
     </>
   );
 };
--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,6 +1,9 @@
+"use client";
 import { cn } from "@/lib/utils";
+import { vapi } from "@/lib/vapi.sdk";
 import Image from "next/image";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 enum CallStatus {
   INACTIVE = "INACTIVE",
@@ -9,23 +12,104 @@ enum CallStatus {
   FINISHED = "FINISHED",
 }
 
+interface SavedMessage {
+  role: "user" | "system" | "assistant";
+  content: string;
+}
+
 const Agent = ({ userName, userId, type }: AgentProps) => {
+  const router = useRouter();
   console.log(userName, userId, type);
 
-  const isSpeaking = true;
-  const callStatus = CallStatus.ACTIVE;
+  const [isSpeaking, setIsSpeaking] = useState(false);
+  const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
+  const [messages, setMessages] = useState<SavedMessage[]>([]);
 
-  const messages = [
-    "What is your name?",
-    "What is your age?",
-    "What is your gender?",
-    "What is your email?",
-    "What is your phone number?",
-    "What is your address?",
-    "What is your city?",
-  ];
+  const latestMessage = messages[messages?.length - 1]?.content; // get the last message of the conversation, so that we can display it as a transcript to the user
 
-  const lastMessage = messages[messages?.length - 1];
+  const isCallInactiveOrFinished =
+    callStatus === CallStatus.INACTIVE || callStatus === CallStatus.FINISHED;
+
+  // ----------------  vapi event handlers start ----------------
+  const onCallStart = () => {
+    setCallStatus(CallStatus.ACTIVE);
+  };
+
+  const onCallEnd = () => {
+    setCallStatus(CallStatus.FINISHED);
+  };
+
+  const onMessage = (message: Message) => {
+    if (message?.type === "transcript" && message?.transcriptType === "final") {
+      const newMessage = {
+        role: message?.role,
+        content: message?.transcript,
+      };
+
+      setMessages((prev) => [...prev, newMessage]);
+    }
+  };
+
+  const onSpeechStart = () => {
+    console.log("speech start");
+    setIsSpeaking(true);
+  };
+
+  const onSpeechEnd = () => {
+    console.log("speech end");
+    setIsSpeaking(false);
+  };
+
+  const onError = (error: Error) => {
+    console.log("Error:", error);
+  };
+
+  // ----------------  vapi event handlers end ----------------
+
+  useEffect(() => {
+    // vapi event listeners start on mount
+    vapi.on("call-start", onCallStart);
+    vapi.on("call-end", onCallEnd);
+    vapi.on("message", onMessage);
+    vapi.on("speech-start", onSpeechStart);
+    vapi.on("speech-end", onSpeechEnd);
+    vapi.on("error", onError);
+
+    //  clearing vapi event listeners on unmount
+    return () => {
+      vapi.off("call-start", onCallStart);
+      vapi.off("call-end", onCallEnd);
+      vapi.off("message", onMessage);
+      vapi.off("speech-start", onSpeechStart);
+      vapi.off("speech-end", onSpeechEnd);
+      vapi.off("error", onError);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (callStatus === CallStatus.FINISHED) {
+      router.push("/interview");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [callStatus]);
+
+  const handleCall = async () => {
+    setCallStatus(CallStatus.CONNECTING);
+
+    // start the conversation with this (NEXT_PUBLIC_VAPI_WORKFLOW_ID) specific ai agent
+    await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!, {
+      variableValues: {
+        username: userName,
+        userid: userId,
+      },
+    });
+  };
+
+  const handleDisconnect = async () => {
+    setCallStatus(CallStatus.FINISHED);
+
+    vapi.stop(); // stop the conversation
+  };
 
   return (
     <>
@@ -64,13 +148,13 @@ const Agent = ({ userName, userId, type }: AgentProps) => {
         <div className="transcript-border">
           <div className="transcript">
             <p
-              key={lastMessage}
+              key={latestMessage}
               className={cn(
                 "transition-opacity duration-500 opacity-0",
                 "animate-fadeIn opacity-100"
               )}
             >
-              {lastMessage}
+              {latestMessage}
             </p>
           </div>
         </div>
@@ -79,7 +163,7 @@ const Agent = ({ userName, userId, type }: AgentProps) => {
       {/* btn to start/end call */}
       <div className="w-full flex justify-center">
         {callStatus !== "ACTIVE" ? (
-          <button className="relative btn-call">
+          <button className="relative btn-call" onClick={handleCall}>
             <span
               className={cn(
                 "absolute animate-ping rounded-full opacity-75",
@@ -88,13 +172,16 @@ const Agent = ({ userName, userId, type }: AgentProps) => {
             />
 
             <span className="relative">
-              {callStatus === "INACTIVE" || callStatus === "FINISHED"
-                ? "Call"
-                : ". . ."}
+              {isCallInactiveOrFinished ? "Call" : ". . ."}
             </span>
           </button>
         ) : (
-          <button className="btn-disconnect cursor-pointer">End</button>
+          <button
+            className="btn-disconnect cursor-pointer"
+            onClick={handleDisconnect}
+          >
+            End
+          </button>
         )}
       </div>
     </>
