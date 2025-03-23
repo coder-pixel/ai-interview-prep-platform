eafaa1824b087063c0ec16658a15d1d0a38c5364
sauvik <sauvik@logic-square.com>
2025-03-23 22:17:00 +0530

    feat: add password visibility toggle in FormField component

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -6,6 +6,9 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
+import { Button } from "./ui/button";
 
 interface FormFieldProps<T extends FieldValues> {
   control: Control<T>;
@@ -22,6 +25,9 @@ const FormField = <T extends FieldValues>({
   placeholder,
   type = "text",
 }: FormFieldProps<T>) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const isPasswordField = type === "password";
+
   return (
     <Controller
       control={control}
@@ -30,12 +36,24 @@ const FormField = <T extends FieldValues>({
         <FormItem>
           <FormLabel className="label">{label}</FormLabel>
           <FormControl>
-            <Input
-              className="input"
-              placeholder={placeholder}
-              {...field}
-              type={type}
-            />
+            <div className="relative">
+              <Input
+                className="input pr-10"
+                placeholder={placeholder}
+                {...field}
+                type={isPasswordField && showPassword ? "text" : type}
+              />
+              {isPasswordField && (
+                <Button
+                  type="button"
+                  variant="ghost"
+                  className="absolute inset-y-0 right-5 top-1/2 -translate-y-1/2 flex items-center cursor-pointer"
+                  onClick={() => setShowPassword((prev) => !prev)}
+                >
+                  {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+                </Button>
+              )}
+            </div>
           </FormControl>
           <FormMessage />
         </FormItem>
